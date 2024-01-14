3052e266adaa9f58d5964fe38735812478524bd0
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-14 10:49:55 +0700

    style: Install Geist as main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
