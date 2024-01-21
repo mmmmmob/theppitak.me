fcbc90481ba2020e29f3fd1ec77e23557262461a
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-22 02:35:15 +0700

    style: Add layout for main Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="h-screen">
-        <div className="absolute left-0 top-0 h-screen w-72 bg-black" />
+    <div className="absolute left-0 top-0 flex h-screen w-72 flex-col bg-black">
+      <div className="flex h-32 flex-grow-0 border-b-2">
+        <div className="mb-3 self-center pl-7">
+          <Image src="/logo.png" alt="logo" width={90} height={90} />
+        </div>
+        <div className="h-fit self-center">
+          <p className=" pl-5 text-2xl font-semibold text-white">
+            Theppitak Maneepong
+          </p>
+        </div>
+      </div>
+      <div className="m-4 flex-1  text-4xl italic text-white">
+        <div className="flex h-[calc(100dvh-240px)] flex-col justify-center">
+          <div className="m-10">About</div>
+          <div className="m-10">Projects</div>
+          <div className="m-10">Blogs</div>
+          <div className="m-10">Contact</div>
+        </div>
       </div>
     </div>
   );
