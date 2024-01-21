815d4c31561548adfac083c53a5cfa260e352e3f
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-21 18:53:10 +0700

    style: Initial layout for Navbar and Footer with support for dark mode switching

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return <div className="z-10 h-12 w-screen bg-black">test</div>;
+};
+
+export default Footer;
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="h-screen">
+        <div className="absolute left-0 top-0 h-screen w-80 bg-black" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
