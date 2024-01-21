6560d825be6c9dd8c730467e82883f158ca1402d
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-21 16:29:45 +0700

    style: Add all font families file to app folder

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-gray-300">
+      <p className="m-4 text-9xl font-semibold text-slate-800">🚧</p>
       <p className="m-4 text-7xl font-semibold text-slate-800">
-        👷🏼 Under Construction...
+        Under Construction
       </p>
+      <p className="m-4 text-7xl text-slate-800">อยู่ระหว่างดำเนินการ</p>
+      <p className="m-4 text-7xl text-slate-800">このサイトは建設中です</p>
+      <p className="m-4 text-7xl text-slate-800">이사이트는공사중입니다</p>
     </div>
   );
 }
