789a84e13637051e776b036069b460b5ce7027a2
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-29 01:14:30 +0700

    feat: Add Cloudflare Turnstile authentication

--- app/api/handler.ts
+++ app/api/handler.ts
@@ -4,15 +4,19 @@ export default async function Handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const form = new URLSearchParams();
-  form.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY);
-  form.append("response", req.body["cf-turnstile-response"]);
-  form.append("remoteip", req.headers["x-forwarded-for"] as string);
+  if (process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY !== undefined) {
+    const form = new URLSearchParams();
+    form.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY);
+    form.append("response", req.body["cf-turnstile-response"]);
+    form.append("remoteip", req.headers["x-forwarded-for"] as string);
 
-  const result = await fetch(
-    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    { method: "POST", body: form },
-  );
-  const json = await result.json();
-  res.status(result.status).json(json);
+    const result = await fetch(
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+      { method: "POST", body: form },
+    );
+    const json = await result.json();
+    res.status(result.status).json(json);
+  } else {
+    console.error("CLOUDFLARE_TURNSTILE_SECRET_KEY is undefined");
+  }
 }
