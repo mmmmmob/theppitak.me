6a89df7101e7f810ba7d50a9dd4e949511c8d60c
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-30 01:47:14 +0700

    style: Add style for navbar to make permanent color to clicked url

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <div className="absolute left-0 top-0 flex h-screen w-72 flex-col bg-black">
       <Link className="flex h-32 flex-grow-0 border-b-2" href="/">
@@ -18,25 +21,41 @@ const Navbar = () => {
       <div className="m-4 flex-1  text-4xl italic text-white">
         <div className="flex h-[calc(100dvh-240px)] flex-col justify-center">
           <Link
-            className="m-10 hover:text-blue hover:underline hover:decoration-red/80"
+            className={`m-10 ${
+              router.pathname === "/about"
+                ? "text-blue underline decoration-red"
+                : "hover:text-blue hover:underline hover:decoration-red/80"
+            }`}
             href="/about"
           >
             About
           </Link>
           <Link
-            className="m-10 hover:text-blue hover:underline hover:decoration-red/80"
+            className={`m-10 ${
+              router.pathname === "/projects"
+                ? "text-blue underline decoration-red"
+                : "hover:text-blue hover:underline hover:decoration-red/80"
+            }`}
             href="/projects"
           >
             Projects
           </Link>
           <Link
-            className="m-10 hover:text-blue hover:underline hover:decoration-red/80"
+            className={`m-10 ${
+              router.pathname === "/blogs"
+                ? "text-blue underline decoration-red"
+                : "hover:text-blue hover:underline hover:decoration-red/80"
+            }`}
             href="/blogs"
           >
             Blogs
           </Link>
           <Link
-            className="m-10 hover:text-blue hover:underline hover:decoration-red/80"
+            className={`m-10 ${
+              router.pathname === "/contact"
+                ? "text-blue underline decoration-red"
+                : "hover:text-blue hover:underline hover:decoration-red/80"
+            }`}
             href="/contact"
           >
             Contact
