072402138dbace979822db9f910d89e8731c5f10
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-02-18 18:31:36 +0700

    feat: Add new layout and info on Bio component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,5 +2,11 @@ import React from "react";
 import Bio from "../components/about/Bio";
 
 export default function About() {
-  return <Bio />;
+  return (
+    <div className="container mx-auto flex h-full flex-col justify-center self-center">
+      <div>
+        <Bio />
+      </div>
+    </div>
+  );
 }
