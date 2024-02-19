88e42c6a4c318a84a43ce16cab27df91757f1bf1
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-02-19 08:53:56 +0700

    build: Install shadcn/ui for carousel and tooltip

--- app/components/about/WorkExp.tsx
+++ app/components/about/WorkExp.tsx
@@ -1,3 +1,10 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import React from "react";
 
 export default function WorkExp() {
@@ -7,13 +14,29 @@ export default function WorkExp() {
         <span className="mr-2 text-4xl">💼</span>
         <span className="text-4xl font-semibold italic">Work Experiences</span>
       </div>
-      <div className="flex  overflow-y-scroll">
-        <div className="mr-4 mt-6 h-36 w-1/3 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
-        <div className="mr-4 mt-6 h-36 w-1/3 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
-        <div className="mr-4 mt-6 h-36 w-1/3 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
-        <div className="mr-4 mt-6 h-36 w-1/3 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
-        <div className="mt-6 h-36 w-1/3 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
-      </div>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem className="basis-2/5">
+            <div className="mr-4 mt-6 h-36 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
+          </CarouselItem>
+          <CarouselItem className="basis-2/5">
+            <div className="mr-4 mt-6 h-36 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
+          </CarouselItem>
+          <CarouselItem className="basis-2/5">
+            <div className="mr-4 mt-6 h-36 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-2/5">
+            <div className="mr-4 mt-6 h-36 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
+          </CarouselItem>
+
+          <CarouselItem className="basis-2/5">
+            <div className="mr-4 mt-6 h-36 flex-shrink-0 rounded-xl border-2 border-red dark:border-blue"></div>
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </main>
   );
 }
