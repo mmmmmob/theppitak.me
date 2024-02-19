b68e7d06add7027eef965fafc183c5301dbc9d93
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-02-19 08:59:41 +0700

    chore: Temporary add placeholder text for projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import ProjectCard from "../components/projects/ProjectCard";
 
 const Projects = () => {
-  return <ProjectCard />;
+  // return <ProjectCard />;
+  return (
+    <div className="flex h-full justify-center">
+      <p className="place-self-center text-center text-6xl font-bold">
+        🏗️ Under Construction
+      </p>
+    </div>
+  );
 };
 
 export default Projects;
